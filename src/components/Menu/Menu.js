ce53a09886f07f71e91122f5f894a1c1d30ed822
hoarus <hoarus@gmail.com>
2022-12-01 07:14:53 -0700

    Add mobile menu bar for smaller screens

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
 export default function Menu(props){
   const selectSection = props.selectSection;
   const selectedSection = props.selectedSection;
+  const [displayMobileMenuItems, setDisplayMobileMenuItems] = useState(false)
+
+  function toggleMobileMenuItems() {
+    console.log(displayMobileMenuItems);
+    switch(displayMobileMenuItems) {
+      case false:
+        setDisplayMobileMenuItems(true);
+        break;
+      case true:
+        setDisplayMobileMenuItems(false);
+      default:
+        setDisplayMobileMenuItems(false);
+    }
+  }
 
 
   return(
+    <Fragment>
     <div className="menu prevent-select">
       <div className={`tab first-tab ${selectedSection==="About" && "selected-tab"}`} onClick={() => selectSection("About")}>About Me</div>
       <div className={`tab ${selectedSection==="Projects" && "selected-tab"}`}onClick={() => selectSection("Projects")}>Projects</div>
@@ -13,5 +28,13 @@ export default function Menu(props){
       <div className={`tab ${selectedSection==="Resume" && "selected-tab"}`}onClick={() => selectSection("Resume")}>Resume</div>
       <div className={`tab ${selectedSection==="Contact" && "selected-tab"}`}onClick={() => selectSection("Contact")}>Contact</div>
     </div>
+    <div className={`menu-mobile ${displayMobileMenuItems===true && "visible-mobile-menu"}`} onClick={() => toggleMobileMenuItems()}>Menu
+      <div className={`tab first-tab ${displayMobileMenuItems===true && "visible-mobile-tab"}`} onClick={() => selectSection("About")}>About Me</div>
+      <div className={`tab ${displayMobileMenuItems===true && "visible-mobile-tab"}`}onClick={() => selectSection("Projects")}>Projects</div>
+      <div className={`tab ${displayMobileMenuItems===true && "visible-mobile-tab"}`}onClick={() => selectSection("Certifications")}>Certifications</div>
+      <div className={`tab ${displayMobileMenuItems===true && "visible-mobile-tab"}`}onClick={() => selectSection("Resume")}>Resume</div>
+      <div className={`tab ${displayMobileMenuItems===true && "visible-mobile-tab"}`}onClick={() => selectSection("Contact")}>Contact</div>
+    </div>
+    </Fragment>
   )
 }
